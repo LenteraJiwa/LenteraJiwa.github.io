a194fc0d8b062fbab689c3d7f3c80d8a0fa414b7
rasydalamsyah-del <rasydalamsyah@gmail.com>
2026-05-27 09:14:11 +0700

    Fix uploadPhoto - hapus setSharing

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@
 
 const LJ_AUTH = (function() {
 
-  const API = 'https://script.google.com/macros/s/AKfycbx-OI7qPl1ZCylFrPWIrVp70rFfuGT89nMJrcOM1TH8H1zpsLSvh_2rUeWC7tV33xsWcQ/exec';
+  const API = 'https://script.google.com/macros/s/AKfycbzByVtfQMbTwOTSS6W-JPbaq0xeoXKJyXFBVr4DOuRr6GC4U-sMcw2FWujiu2gZTv756A/exec';
   const KEY_SESSION = 'lj_session';
 
   /* ── Kirim request ke Apps Script ── */
